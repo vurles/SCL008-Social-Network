4df1d3d7fc487254fa9f8e54e96248520d2132c1
vurles <vurles27@gmail.com>
2019-03-26 17:14:23 -0300

    instalando dependencias, carpetas y archivos necesarios para el proyecto

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/src'));
+
+app.get('/',function(request, response){
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, function(){
+  console.log('Server Express Ready!');
+});
