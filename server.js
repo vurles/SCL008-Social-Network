7f59c730d2f9196e1a1ceaf7345f267cf5b18509
vurles <vurles27@gmail.com>
2019-03-29 15:01:18 -0300

    crear cuenta con email y contraceña mas autentificacion de usuario en firebase

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/',function(request, response){
   response.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function(){
+app.listen(3000, function(){// si el servidor se queda pegado podemos cambiar el 3000 por 4200 o reiniciar el pc
   console.log('Server Express Ready!');
 });
