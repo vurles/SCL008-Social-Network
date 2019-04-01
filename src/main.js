23a92caf76ceea2c52e75887edf0387a7f78f5df
vurles <vurles27@gmail.com>
2019-04-01 17:07:50 -0300

    login con google

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 // inicializacion de las rutas firebase
+var config = {
+    apiKey: "AIzaSyD36cB7TFje_JTuaP53m_Q4sJJirK-hLRg",
+    authDomain: "tinder-para-mascotas.firebaseapp.com",
+    databaseURL: "https://tinder-para-mascotas.firebaseio.com",
+    projectId: "tinder-para-mascotas",
+    storageBucket: "tinder-para-mascotas.appspot.com",
+    messagingSenderId: "236093972910"
+  };
+  firebase.initializeApp(config);
+  
 import {initRout} from './route.js';
 
 const init = () => {
