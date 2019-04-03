ec54208cb0455a338363ef12ba8abd099fad2b68
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-04-03 17:54:01 -0300

    MAIN.CSS agregando margin-top al login y create y cambiando el tamaño de las letras

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -5,7 +5,7 @@
 
   
 
-export const login = () => {
+  export const login = () => {
     let provider = new firebase.auth.GoogleAuthProvider();
     //provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
     firebase.auth().signInWithPopup(provider)
@@ -42,14 +42,4 @@ export const acount = () => {
   });
 
 }
-// esto nos permite enviar un vinculo de autentificacion al correo
-// firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
-//   .then(function() {
-//     // The link was successfully sent. Inform the user.
-//     // Save the email locally so you don't need to ask the user for it again
-//     // if they open the link on the same device.
-//     window.localStorage.setItem('emailForSignIn', email);
-//   })
-//   .catch(function(error) {
-//     // Some error occurred, you can inspect the code: error.code
-//   });
+
