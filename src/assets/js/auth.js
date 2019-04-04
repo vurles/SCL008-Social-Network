b9fd0cfd5d054b6a15ccbb0768728ba0bdee5c84
FabyTapia <ftapiamorales@gmail.com>
2019-04-04 17:01:21 -0300

    resolucion de conflictos

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -104,3 +104,18 @@ user.sendEmailVerification().then(function() {
   alert('Usuario ya registrado');
 });
 }
+<<<<<<< Updated upstream
+=======
+// esto nos permite enviar un vinculo de autentificacion al correo
+// firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+//   .then(function() {
+//     // The link was successfully sent. Inform the user.
+//     // Save the email locally so you don't need to ask the user for it again
+//     // if they open the link on the same device.
+//     window.localStorage.setItem('emailForSignIn', email);
+//   })
+//   .catch(function(error) {
+//     // Some error occurred, you can inspect the code: error.code
+//   });
+
+>>>>>>> Stashed changes
