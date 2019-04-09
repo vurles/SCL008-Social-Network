00cfb10ecff8bd919da059478fa0f8a4419758d3
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-04-09 16:35:13 -0400

    ALERT.JS agregando función validateData y validateInput y AUTH.SPEC.JS agregando test de las funciones creadas en ALERT.JS

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,4 +1,4 @@
-import { validateMail } from './../src/assets/js/alert.js';
+import { validateMail, validateInput, validateData } from './../src/assets/js/alert.js';
 
 describe('validateMail', () => {
     it('Debería retornar false, si el correo no es valido', () => {
@@ -9,5 +9,29 @@ describe('validateMail', () => {
 describe('validateMail', () => {
     it('Debería retornar true, si el correo es valido', () => {
         expect(validateMail('ftapiamorales@gmail.com')).toBe(true);
+    })
+})
+
+describe('validateInput', () => {
+    it('Debería retornar false, si los input para iniciar sesión estan vacíos', () => {
+        expect(validateInput('','')).toBe(false);
+    })
+})
+
+describe('validateInput', () => {
+    it('Debería retornar true, si los input para iniciar sesión estan completos', () => {
+        expect(validateInput('ftapiamorales@gmail.com','123456')).toBe(true);
+    })
+})
+
+describe('validateData', () => {
+    it('Debería retornar false, si los input para registrarse estan vacíos', () => {
+        expect(validateData('','','','')).toBe(false);
+    })
+})
+
+describe('validateData', () => {
+    it('Debería retornar true, si los input para registrarse estan completos', () => {
+        expect(validateData('Amanda','Perro','ftapiamorales@gmail.com','123456')).toBe(true);
     })
 })
